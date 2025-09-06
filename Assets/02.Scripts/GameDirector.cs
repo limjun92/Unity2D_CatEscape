5d6d520a7fdc7bbf2e57a3933c956ab5adfad733
Junhyeong Lim <52904572+limjun92@users.noreply.github.com>
2025-09-06 13:20:45 +0900

    four commit

--- Assets/02.Scripts/GameDirector.cs
+++ Assets/02.Scripts/GameDirector.cs
@@ -6,9 +6,19 @@ using UnityEngine.UI;
 public class GameDirector : MonoBehaviour
 {
     public Image hpGauge;
+    public Text gameStatus;
 
     public void DecreaseHp()
     {
         hpGauge.fillAmount -= 0.1f;
+    }
+
+    void Update()
+    {
+        if(hpGauge.fillAmount <= 0)
+        {
+            gameStatus.text = "Game Over";
+            Time.timeScale = 0;
+        }
     }
 }
